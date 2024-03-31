00343bb38ce13a9d41f7c52514eeccc9e63ba068
stefanperetzki <spe@ndcom.de>
2024-03-31 22:14:59 +0200

    update

---
+++ src/DependencyInjection/InndvidiuellHideAdminActionsContaoExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inndividuell\HideAdminActions\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class InndvidiuellHideAdminActionsContaoExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        (new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config')))
+            ->load('services.yaml')
+        ;
+    }
+}
